24b391a9ae12d0d8415404f775c1f51250d4160b
tatiana_alekseeva <tatianaalekseeva1313@gmail.com>
2022-12-12 14:47:23 +0300

    Задание 3

--- src/Bird.java
+++ src/Bird.java
@@ -1,48 +1,48 @@
-import java.util.Objects;
-
-public class Bird extends Animal {
-    String  habitat;
-
-    public Bird(String nickname, int amountYear, String habitat) {
-        super(nickname, amountYear);
-        setHabitat(habitat);
-    }
-
-    public String getHabitat() {
-        return habitat;
-    }
-
-    public void setHabitat(String habitat) {
-        if(habitat != null && !habitat.isEmpty()) {
-            this.habitat = habitat;
-        }
-    }
-
-    @Override
-    public void eat() {
-        System.out.println("Охотиться");
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        if (!super.equals(o)) return false;
-        Bird bird = (Bird) o;
-        return Objects.equals(getHabitat(), bird.getHabitat()) && super.equals(o);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), getHabitat());
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + ". Среда обитания: " + habitat;
-    }
-}
+//import java.util.Objects;
+//
+//public class Bird extends Animal {
+//    String  habitat;
+//
+//    public Bird(String nickname, int amountYear, String habitat) {
+//        super(nickname, amountYear);
+//        setHabitat(habitat);
+//    }
+//
+//    public String getHabitat() {
+//        return habitat;
+//    }
+//
+//    public void setHabitat(String habitat) {
+//        if(habitat != null && !habitat.isEmpty()) {
+//            this.habitat = habitat;
+//        }
+//    }
+//
+//    @Override
+//    public void eat() {
+//        System.out.println("Охотиться");
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) {
+//            return true;
+//        }
+//        if (o == null || getClass() != o.getClass()) {
+//            return false;
+//        }
+//        if (!super.equals(o)) return false;
+//        Bird bird = (Bird) o;
+//        return Objects.equals(getHabitat(), bird.getHabitat()) && super.equals(o);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(super.hashCode(), getHabitat());
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return super.toString() + ". Среда обитания: " + habitat;
+//    }
+//}
--- src/Herbivore.java
+++ src/Herbivore.java
@@ -1,48 +1,48 @@
-import java.util.Objects;
-
-public class Herbivore extends Mammal {
-    String food;
-
-    public Herbivore(String nickname, int amountYear, String habitat, int speedMovement, String food) {
-        super(nickname, amountYear, habitat, speedMovement);
-        setFood(food);
-    }
-
-    public String getFood() {
-        return food;
-    }
-
-    public void setFood(String food) {
-        if(food != null && !food.isEmpty()) {
-            this.food = food;
-        }
-    }
-
-    @Override
-    public void eat() {
-        System.out.println("Пастись");
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        if (!super.equals(o)) return false;
-        Herbivore herbivore = (Herbivore) o;
-        return Objects.equals(getFood(), herbivore.getFood()) && super.equals(o);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), getFood());
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + ". Пища: " + food;
-    }
-}
+//import java.util.Objects;
+//
+//public class Herbivore extends Mammal {
+//    String food;
+//
+//    public Herbivore(String nickname, int amountYear, String habitat, int speedMovement, String food) {
+//        super(nickname, amountYear, habitat, speedMovement);
+//        setFood(food);
+//    }
+//
+//    public String getFood() {
+//        return food;
+//    }
+//
+//    public void setFood(String food) {
+//        if(food != null && !food.isEmpty()) {
+//            this.food = food;
+//        }
+//    }
+//
+//    @Override
+//    public void eat() {
+//        System.out.println("Пастись");
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) {
+//            return true;
+//        }
+//        if (o == null || getClass() != o.getClass()) {
+//            return false;
+//        }
+//        if (!super.equals(o)) return false;
+//        Herbivore herbivore = (Herbivore) o;
+//        return Objects.equals(getFood(), herbivore.getFood()) && super.equals(o);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(super.hashCode(), getFood());
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return super.toString() + ". Пища: " + food;
+//    }
+//}
